ef394e72b3415f8b4323db4bee8a405fd13be07c
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2022-05-06 11:03:17 +0200

    refactor: 💡 disable query logger in test

--- apps/sandbox-angular/src/app/query/query.core.ts
+++ apps/sandbox-angular/src/app/query/query.core.ts
@@ -4,13 +4,13 @@ import { environment } from '../../environments/environment';
 export const query = initializeQuery({
   baseRoute: environment.apiUrl,
   logging: {
-    queryStateChanges: !environment.production,
+    queryStateChanges: !environment.production && !environment.test,
   },
 });
 
 export const ggQuery = initializeQuery({
   baseRoute: environment.ggApiUrl,
   logging: {
-    queryStateChanges: !environment.production,
+    queryStateChanges: !environment.production && !environment.test,
   },
 });
--- apps/sandbox-angular/src/environments/environment.prod.ts
+++ apps/sandbox-angular/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  test: false,
   apiUrl: 'https://jsonplaceholder.typicode.com',
   ggApiUrl: 'https://api.fifa.gg',
 };
